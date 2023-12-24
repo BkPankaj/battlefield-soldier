dde8cb84ed84cf85f63bf8c4160ae1e7fa2b988b
BkPankaj <borade.keshav825@gmail.com>
2023-12-24 23:30:44 +0530

    frontend added

---
+++ battlefield/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BrowserRouter, Routes,Route} from "react-router-dom";
+import Menu from './pages/Menu/Menu'
+const App = () => {
+  return( 
+  <BrowserRouter basename='/'> 
+  <Routes>
+  <Route path="/" element={<Menu/>} />
+  </Routes>
+  
+   </BrowserRouter>
+  )
+}
+
+export default App;
